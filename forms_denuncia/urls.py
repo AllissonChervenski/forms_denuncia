446084fe4201ad4ca5df57d9554251ec4e0272c8
allissonchervenski <allissonchervenski5@gmail.com>
2026-08-21 06:50:44 -0300

    fix: habilita servico de arquivos de media em producao e estiliza evidencias

--- forms_denuncia/urls.py
+++ forms_denuncia/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
-
-
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('dashboard/', include('dashboard.urls')),
-
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': False}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
